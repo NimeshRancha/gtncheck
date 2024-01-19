58f109ed247c223c6ebc6235f2cd7e60844b3bf4
r.nimesh <r.nimesh@gtngroup.com>
2024-01-19 16:06:39 +0530

    Initial Commit

---
+++ swiftfulThinking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftfulThinking
+//
+//  Created by Nimesh Ranchagoda on 2024-01-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftfulThinking/swiftfulThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulThinkingApp.swift
+//  swiftfulThinking
+//
+//  Created by Nimesh Ranchagoda on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct swiftfulThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
